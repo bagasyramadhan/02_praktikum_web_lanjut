96cd98c1879383c1d9da045188bb429fd5887fec
bagasyramadhan <bagas99banihasyim@gmail.com>
2023-02-24 15:41:41 +0700

    commit ketiga

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
-use App\Http\Controllers\PageController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\ArticleController;
 use Illuminate\Support\Facades\Route;
 
 
@@ -29,7 +31,6 @@ use Illuminate\Support\Facades\Route;
 // Route::get('/articles/{id}', function ($id) {
 // echo "Ini merupakan halaman artikel dengan id $id";
 // });
-
-Route::get('/', [PageController::class, 'index']);
-Route::get('/about', [PageController::class, 'about']);
-Route::get('/articles/{id}', [PageController::class, 'articles']);
+Route::get('/', [HomeController::class, 'index']);
+Route::get('/about', [AboutController::class, 'about']);
+Route::get('/articles/{id}', [ArticleController::class, 'articles']);
